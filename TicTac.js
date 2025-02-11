898e256bbb8e677d3c02dc4941a78f3dfc284fe3
Tsalu19 <tsantamaria@fwdcostarica.com>
2025-02-11 15:24:44 -0600

    Gatito

--- TicTac.js
+++ TicTac.js
@@ -1,4 +1,6 @@
-const cell= document.getElementsByClassName("celda")
+const celda= document.getElementsByClassName("cell")
+    console.log(celda)
 const Reset = document.getElementById("reset")
 const Notification = document.getElementById("notification")
+
 
